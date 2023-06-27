b562fb26c0489748a566c6ddf536509ae34591ee
GrishmaKhanal <grishmakhanal48@gmail.com>
2023-06-27 18:09:19 +0545

    fib done

--- fibonacci/client/Client.java
+++ fibonacci/client/Client.java
@@ -1,14 +1,16 @@
 package fibonacci.client;
 import java.rmi.Naming;
+import java.util.Scanner;
 
 import fibonacci.server.Fibonacci;
 public class Client {
     public static void main(String[] args){
         try{
+            Scanner scanner = new Scanner(System.in);
             Fibonacci fibonacci = (Fibonacci) Naming.lookup("rmi://localhost/FibonacciService");
-            System.out.println("Fibonacci(5): " + fibonacci.fib(5));
-            System.out.println("Fibonacci(10): " + fibonacci.fib(10));
-            System.out.println("Fibonacci(15): " + fibonacci.fib(15));
+            System.out.print("Enter a n for Fibonacci(n): ");
+            int n = scanner.nextInt();
+            System.out.println("Fibonacci("+ n +"): " + fibonacci.fib(n));
         } catch(Exception e){
             System.err.println("Client exception: " + e.toString());
             e.printStackTrace();
